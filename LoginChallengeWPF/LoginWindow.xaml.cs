d7d35d8cbe33a81a88cca9915636eb81241be2fd
Juan Ignacio <jimartinezabadias@gmail.com>
2021-04-19 12:08:20 -0300

    First commit

---
+++ LoginChallengeWPF/LoginWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginChallengeWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new Exception("Saliendo de la aplicacion");
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new Exception("Ingresando a la aplicacion");
+        }
+
+    }
+}
